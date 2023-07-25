ffbbf75850b81110e85f5cc8dcd2b3d5fa3e23c4
Laird Nelson <ljnelson@gmail.com>
2023-07-25 13:43:49 -0700

    General version refresh (#1)
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- src/main/java/io/github/ljnelson/oci/secrets/configsource/SelectiveBuilderFunction.java
+++ src/main/java/io/github/ljnelson/oci/secrets/configsource/SelectiveBuilderFunction.java
@@ -1,17 +1,14 @@
 /*
- * Copyright © 2022 Laird Nelson.
+ * Copyright © 2022–2023 Laird Nelson.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package io.github.ljnelson.oci.secrets.configsource;
 
@@ -25,12 +22,10 @@ import java.util.regex.Pattern;
 import com.oracle.bmc.secrets.requests.GetSecretBundleRequest;
 
 /**
- * A decorator {@link Function} that guards another similar {@link
- * Function} with a {@link Predicate} controlling when it will be
- * executed.
+ * A decorator {@link Function} that guards another similar {@link Function} with a {@link Predicate} controlling when
+ * it will be executed.
  *
- * @author <a href="https://about.me/lairdnelson/" target="_top">Laird
- * Nelson</a>
+ * @author <a href="https://about.me/lairdnelson/" target="_top">Laird Nelson</a>
  *
  * @see #apply(String)
  */
@@ -55,22 +50,16 @@ public final class SelectiveBuilderFunction implements Function<String, GetSecre
     /**
      * Creates a new {@link SelectiveBuilderFunction}.
      *
-     * @param builderFunction a {@link Function} that, {@linkplain
-     * Function#apply(Object) when supplied with a MicroProfile Config
-     * property name}, returns a fully-configured {@link
-     * GetSecretBundleRequest.Builder} suitable for the property name,
-     * or {@code null} if the property name is unsuitable; will only
-     * be executed if the property name is non-{@code null} and
-     * {@linkplain Matcher#matches() matched} by the supplied {@link
-     * Pattern}; must not be {@code null}
-     *
-     * @param validPropertyNamesPattern a {@link Pattern} used to
-     * {@linkplain Matcher#matches() match} property names for further
-     * processing by the supplied {@code builderFunction}; must not be
+     * @param builderFunction a {@link Function} that, {@linkplain Function#apply(Object) when supplied with a
+     * MicroProfile Config property name}, returns a fully-configured {@link GetSecretBundleRequest.Builder} suitable
+     * for the property name, or {@code null} if the property name is unsuitable; will only be executed if the property
+     * name is non-{@code null} and {@linkplain Matcher#matches() matched} by the supplied {@link Pattern}; must not be
      * {@code null}
      *
-     * @exception NullPointerException if either argument is {@code
-     * null}
+     * @param validPropertyNamesPattern a {@link Pattern} used to {@linkplain Matcher#matches() match} property names
+     * for further processing by the supplied {@code builderFunction}; must not be {@code null}
+     *
+     * @exception NullPointerException if either argument is {@code null}
      *
      * @see #SelectiveBuilderFunction(Function, Predicate)
      *
@@ -87,23 +76,17 @@ public final class SelectiveBuilderFunction implements Function<String, GetSecre
     /**
      * Creates a new {@link SelectiveBuilderFunction}.
      *
-     * @param builderFunction a {@link Function} that, {@linkplain
-     * Function#apply(Object) when supplied with a MicroProfile Config
-     * property name}, returns a fully-configured {@link
-     * GetSecretBundleRequest.Builder} suitable for the property name,
-     * or {@code null} if the property name is unsuitable; will only
-     * be executed if the property name is {@linkplain
-     * String#equals(Object) equal to} the supplied {@code
-     * validPropertyName0} argument; must not be {@code null}
+     * @param builderFunction a {@link Function} that, {@linkplain Function#apply(Object) when supplied with a
+     * MicroProfile Config property name}, returns a fully-configured {@link GetSecretBundleRequest.Builder} suitable
+     * for the property name, or {@code null} if the property name is unsuitable; will only be executed if the property
+     * name is {@linkplain String#equals(Object) equal to} the supplied {@code validPropertyName0} argument; must not be
+     * {@code null}
      *
-     * @param validPropertyName0 a MicroProfile Config property name
-     * to which any property name supplied to the {@link
-     * #apply(String)} method must be equal in order for a non-{@code
-     * null} {@link GetSecretBundleRequest.Builder} to be returned by
-     * it; must not be {@code null}
+     * @param validPropertyName0 a MicroProfile Config property name to which any property name supplied to the {@link
+     * #apply(String)} method must be equal in order for a non-{@code null} {@link GetSecretBundleRequest.Builder} to be
+     * returned by it; must not be {@code null}
      *
-     * @exception NullPointerException if either argument is {@code
-     * null}
+     * @exception NullPointerException if either argument is {@code null}
      *
      * @see #SelectiveBuilderFunction(Function, Set)
      *
@@ -118,23 +101,17 @@ public final class SelectiveBuilderFunction implements Function<String, GetSecre
     /**
      * Creates a new {@link SelectiveBuilderFunction}.
      *
-     * @param builderFunction a {@link Function} that, {@linkplain
-     * Function#apply(Object) when supplied with a MicroProfile Config
-     * property name}, returns a fully-configured {@link
-     * GetSecretBundleRequest.Builder} suitable for the property name,
-     * or {@code null} if the property name is unsuitable; will only
-     * be executed if the property name is {@linkplain
-     * String#equals(Object) equal to} one of the supplied valid
-     * property names; must not be {@code null}
-     *
-     * @param validPropertyName0 a MicroProfile Config property name;
-     * must not be {@code null}
-     *
-     * @param validPropertyName1 a MicroProfile Config property name;
-     * must not be {@code null}
-     *
-     * @exception NullPointerException if any argument is {@code
+     * @param builderFunction a {@link Function} that, {@linkplain Function#apply(Object) when supplied with a
+     * MicroProfile Config property name}, returns a fully-configured {@link GetSecretBundleRequest.Builder} suitable
+     * for the property name, or {@code null} if the property name is unsuitable; will only be executed if the property
+     * name is {@linkplain String#equals(Object) equal to} one of the supplied valid property names; must not be {@code
      * null}
+     *
+     * @param validPropertyName0 a MicroProfile Config property name; must not be {@code null}
+     *
+     * @param validPropertyName1 a MicroProfile Config property name; must not be {@code null}
+     *
+     * @exception NullPointerException if any argument is {@code null}
      *
      * @see #SelectiveBuilderFunction(Function, Set)
      *
@@ -151,23 +128,17 @@ public final class SelectiveBuilderFunction implements Function<String, GetSecre
     /**
      * Creates a new {@link SelectiveBuilderFunction}.
      *
-     * @param builderFunction a {@link Function} that, {@linkplain
-     * Function#apply(Object) when supplied with a MicroProfile Config
-     * property name}, returns a fully-configured {@link
-     * GetSecretBundleRequest.Builder} suitable for the property name,
-     * or {@code null} if the property name is unsuitable; will only
-     * be executed if the property name is {@linkplain
-     * String#equals(Object) equal to} one of the supplied valid
-     * property names; must not be {@code null}
+     * @param builderFunction a {@link Function} that, {@linkplain Function#apply(Object) when supplied with a
+     * MicroProfile Config property name}, returns a fully-configured {@link GetSecretBundleRequest.Builder} suitable
+     * for the property name, or {@code null} if the property name is unsuitable; will only be executed if the property
+     * name is {@linkplain String#equals(Object) equal to} one of the supplied valid property names; must not be {@code
+     * null}
      *
-     * @param validPropertyName0 a MicroProfile Config property name;
-     * must not be {@code null}
+     * @param validPropertyName0 a MicroProfile Config property name; must not be {@code null}
      *
-     * @param validPropertyName1 a MicroProfile Config property name;
-     * must not be {@code null}
+     * @param validPropertyName1 a MicroProfile Config property name; must not be {@code null}
      *
-     * @param validPropertyName2 a MicroProfile Config property name;
-     * must not be {@code null}
+     * @param validPropertyName2 a MicroProfile Config property name; must not be {@code null}
      *
      * @exception NullPointerException if any argument is {@code null}
      *
@@ -188,24 +159,17 @@ public final class SelectiveBuilderFunction implements Function<String, GetSecre
     /**
      * Creates a new {@link SelectiveBuilderFunction}.
      *
-     * @param builderFunction a {@link Function} that, {@linkplain
-     * Function#apply(Object) when supplied with a MicroProfile Config
-     * property name}, returns a fully-configured {@link
-     * GetSecretBundleRequest.Builder} suitable for the property name,
-     * or {@code null} if the property name is unsuitable; will only
-     * be executed if the property name is {@linkplain
-     * String#equals(Object) equal to} one of the elements in the
-     * supplied {@code validPropertyNames} {@link Set}; must not be
-     * {@code null}
+     * @param builderFunction a {@link Function} that, {@linkplain Function#apply(Object) when supplied with a
+     * MicroProfile Config property name}, returns a fully-configured {@link GetSecretBundleRequest.Builder} suitable
+     * for the property name, or {@code null} if the property name is unsuitable; will only be executed if the property
+     * name is {@linkplain String#equals(Object) equal to} one of the elements in the supplied {@code
+     * validPropertyNames} {@link Set}; must not be {@code null}
      *
-     * @param validPropertyNames a {@link Set} of MicroProfile Config
-     * property names for which this {@link SelectiveBuilderFunction}
-     * will potentially {@linkplain #apply(String) respond} with a
-     * non-{@code null} {@link GetSecretBundleRequest.Builder}; must
-     * not be {@code null}
+     * @param validPropertyNames a {@link Set} of MicroProfile Config property names for which this {@link
+     * SelectiveBuilderFunction} will potentially {@linkplain #apply(String) respond} with a non-{@code null} {@link
+     * GetSecretBundleRequest.Builder}; must not be {@code null}
      *
-     * @exception NullPointerException if either argument is {@code
-     * null}
+     * @exception NullPointerException if either argument is {@code null}
      *
      * @see #SelectiveBuilderFunction(Function, Predicate)
      *
@@ -221,22 +185,17 @@ public final class SelectiveBuilderFunction implements Function<String, GetSecre
     /**
      * Creates a new {@link SelectiveBuilderFunction}.
      *
-     * @param builderFunction a {@link Function} that, {@linkplain
-     * Function#apply(Object) when supplied with a MicroProfile Config
-     * property name}, returns a fully-configured {@link
-     * GetSecretBundleRequest.Builder} suitable for the property name,
-     * or {@code null} if the property name is unsuitable; will only
-     * be executed if the supplied {@code propertyNameValidator}
-     * returns {@code true} {@linkplain Predicate#test(Object) when
-     * supplied with the same property name}; must not be {@code null}
+     * @param builderFunction a {@link Function} that, {@linkplain Function#apply(Object) when supplied with a
+     * MicroProfile Config property name}, returns a fully-configured {@link GetSecretBundleRequest.Builder} suitable
+     * for the property name, or {@code null} if the property name is unsuitable; will only be executed if the supplied
+     * {@code propertyNameValidator} returns {@code true} {@linkplain Predicate#test(Object) when supplied with the same
+     * property name}; must not be {@code null}
      *
-     * @param propertyNameValidator a {@link Predicate} that, when
-     * supplied with a MicroProfile Config property name, returns
-     * {@code true} if the supplied {@code builderFunction} should be
-     * executed with the same property name; must not be {@code null}
+     * @param propertyNameValidator a {@link Predicate} that, when supplied with a MicroProfile Config property name,
+     * returns {@code true} if the supplied {@code builderFunction} should be executed with the same property name; must
+     * not be {@code null}
      *
-     * @exception NullPointerException if either argument is {@code
-     * null}
+     * @exception NullPointerException if either argument is {@code null}
      *
      * @see #apply(String)
      */
@@ -254,22 +213,17 @@ public final class SelectiveBuilderFunction implements Function<String, GetSecre
 
 
     /**
-     * If the {@code propertyNameValidator} {@linkplain
-     * #SelectiveBuilderFunction(Function, Predicate) supplied at
-     * construction time} returns {@code true} when supplied with the
-     * supplied {@code propertyName}, then the {@code builderFunction}
-     * {@linkplain #SelectiveBuilderFunction(Function, Predicate)
-     * supplied at construction time} is executed with the same {@code
-     * propertyName} and its return value is returned.
+     * If the {@code propertyNameValidator} {@linkplain #SelectiveBuilderFunction(Function, Predicate) supplied at
+     * construction time} returns {@code true} when supplied with the supplied {@code propertyName}, then the {@code
+     * builderFunction} {@linkplain #SelectiveBuilderFunction(Function, Predicate) supplied at construction time} is
+     * executed with the same {@code propertyName} and its return value is returned.
      *
      * <p>In all other cases this method returns {@code null}.</p>
      *
-     * @param propertyName a MicroProfile Config property name; may be
-     * {@code null}
+     * @param propertyName a MicroProfile Config property name; may be {@code null}
      *
-     * @return a fully-configured {@link
-     * GetSecretBundleRequest.Builder} suitable for the supplied
-     * {@code propertyName}, or {@code null}
+     * @return a fully-configured {@link GetSecretBundleRequest.Builder} suitable for the supplied {@code propertyName},
+     * or {@code null}
      *
      * @see #SelectiveBuilderFunction(Function, Predicate)
      */
--- src/main/java/io/github/ljnelson/oci/secrets/configsource/SimpleSecretsSupplier.java
+++ src/main/java/io/github/ljnelson/oci/secrets/configsource/SimpleSecretsSupplier.java
@@ -1,17 +1,14 @@
 /*
- * Copyright © 2022 Laird Nelson.
+ * Copyright © 2022–2023 Laird Nelson.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package io.github.ljnelson.oci.secrets.configsource;
 
@@ -29,8 +26,7 @@ import com.oracle.bmc.secrets.SecretsClient;
 /**
  * A {@link Supplier} of {@link Secrets} instances.
  *
- * <a href="https://about.me/lairdnelson/" target="_top">Laird
- * Nelson</a>
+ * <a href="https://about.me/lairdnelson/" target="_top">Laird Nelson</a>
  */
 public final class SimpleSecretsSupplier implements Supplier<Secrets> {
 
@@ -75,8 +71,7 @@ public final class SimpleSecretsSupplier implements Supplier<Secrets> {
     /**
      * Creates a new {@link SimpleSecretsSupplier}.
      *
-     * @param adpSupplier a {@link Supplier} of {@link
-     * AbstractAuthenticationDetailsProvider} instances; must not be
+     * @param adpSupplier a {@link Supplier} of {@link AbstractAuthenticationDetailsProvider} instances; must not be
      * {@code null}
      *
      * @exception NullPointerException if {@code adpSupplier} is {@code null}
@@ -92,12 +87,10 @@ public final class SimpleSecretsSupplier implements Supplier<Secrets> {
     /**
      * Creates a new {@link SimpleSecretsSupplier}.
      *
-     * @param builderSupplier a {@link Supplier} of non-{@code null}
-     * {@link SecretsClient.Builder} instances; must not be {@code
-     * null}
+     * @param builderSupplier a {@link Supplier} of non-{@code null} {@link SecretsClient.Builder} instances; must not
+     * be {@code null}
      *
-     * @param adpSupplier a {@link Supplier} of {@link
-     * AbstractAuthenticationDetailsProvider} instances; must not be
+     * @param adpSupplier a {@link Supplier} of {@link AbstractAuthenticationDetailsProvider} instances; must not be
      * {@code null}
      *
      * @exception NullPointerException if either argument is {@code null}
